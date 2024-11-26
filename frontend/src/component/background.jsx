13f3d5f3d5652526410c4a5fd2bfdaa202ae80a8
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2024-11-26 13:02:51 +0300

    my first commit

--- frontend/src/component/background.jsx
+++ frontend/src/component/background.jsx
@@ -1,12 +1,12 @@
 import { useTexture } from "@react-three/drei";
 
 export default function Bg() {
-     const texture = useTexture("/images/background.jpg");
+     const texture = useTexture("/images/bgh.jpg");
    
      return (
        <mesh position={[0, 1.5, -4]} scale={[1.2, 1.2, 1.2]}>
          <planeBufferGeometry />
-         <meshBasicMaterial map={texture} />
+         <meshBasicMaterial map={texture} scale={2} />
        </mesh>
      );
    }
